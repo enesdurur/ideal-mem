82437d51125f66d55bf170f6cddf80bf8ebf72a8
Enes Durur <enes_durur@hotmail.com>
2023-09-15 17:11:08 +0300

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -55,6 +55,28 @@ const projectList = [
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
+const projectList = [
+  {
+    title: "10 Things to know about Azure Static Web Apps 🎉",
+    description: "Collaboration to create a beginner friendly....",
+    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+  },
+  {
+    title: "Web Development for Beginners",
+    description: "Contributed sketch note imagery to accompany...",
+    url: "https://github.com/microsoft/web-dev-for-beginners",
+  },
+  {
+    title: "My Resume Site",
+    description: "Created from Microsoft's resume workshop...",
+    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+  },
+  {
+    title: "GitHub Codespaces and GitHub.dev",
+    description: "Video interview to explain when to use GitHub.dev...",
+    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+  },
+];
 
 const Portfolio = () => {
   return (
